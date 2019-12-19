8062e9127155dc654172136e013ffe7c6e9f615b
lis <pportella23@gmail.com>
2019-12-19 09:53:59 -0300

    backend done

--- server.js
+++ server.js
@@ -1,86 +1,31 @@
 // importando os pacotes para uso no arquivo index.js
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-//const morgan = require('morgan');
-//const bodyParser = require('body-parser');
-//const braille = require('./dictionary');
 
 // crio um servidor express
 const server = express();
 
-// aplico configurações para dentro do servidor express, adicionando middlewares (body-parser, morgan, cors)
-//server.use(morgan('dev'));
-
-//server.use(bodyParser.urlencoded({ extended: false }));
-//server.use(bodyParser.json());
 server.use(cors());
-//server.use(express.static(__dirname + '/front/'));
-//server.use(express.static(path.join(__dirname, '/front')));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true}));
 
-
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-
-
+let result = "";
 // criação de rota que será acessada utilizando o método HTTP GET/
-// http://localhost:9000/
-server.get('/payload', (req, res) => {
-  return res.json({ payload });
-});
 
 
-// server.get('/', (req, res) => {
-//   return res.sendFile('index.html')
-//   //return res.render('/front/index.html');
-// });
-server.get('/add', (req,res) =>{
-  //return res.;
+server.get('/payload', (req,res) =>{
+  return res.json({result});
 })
 
 
 // criação de rota que será acessada utilizando o método HTTP POST/
 // http://localhost:9000/add
 server.post('/add', (req, res) => {
-  const textInput = req.body; //.content  //aqui deve-se passar tambem o .content (propriedade que vamos pegar para conversao)
   
-  console.log(req);
-  
-  if (!textInput) {
-    return res.status(400).end();
-  }
+  result = req.body;
 
-  
-  //const text = textInput.content;
-  //braille.toBraille(result);
-  
-  //payload.push(braille.toBraille(textInput));
-  //console.log(textInput);
-  return res.json(textInput);
 });
 
 // o servidor irá rodar dentro da porta 9000
-server.listen(9000, () => console.log('Express started at http://localhost:9000'));
-
-
-
-            //      TESTAR ESSA POSSIBILIDADE
-
-// const button = document.getElementById('post-btn');
-
-// button.addEventListener('click', async _ => {
-//   try {     
-//     const response = await fetch('yourUrl', {
-//       method: 'post',
-//       body: {
-//         // Your body
-//       }
-//     });
-//     console.log('Completed!', response);
-//   } catch(err) {
-//     console.error(`Error: ${err}`);
-//   }
-// });
-
-        //      E O HTML SERIA
-
-//<button id="post-btn">I'm a button</button>
+server.listen(3000, () => console.log('Express started at http://localhost:3000'));
